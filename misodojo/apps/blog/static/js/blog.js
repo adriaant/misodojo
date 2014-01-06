a4dd3be1073c38b05b4c9fa3b97dcf1651ec5207
Adriaan Tijsseling <adriaan@tijsseling.com>
2014-01-06 22:44:11 +0100

    Add infinite scrolling.

---
+++ misodojo/apps/blog/static/js/blog.js
@@ -0,0 +1,46 @@
+var Blog = {
+
+    loading: false,  // to prevent hitting the ajax function more than necessary
+    num_loaded: 0,   // number of posts already shown
+
+    init: function() {
+        // record # of posts we already have
+        Blog.num_loaded = $(".post").length;
+
+        // hide the non-js pager
+        $('ul.pager').hide();
+
+        // set the trigger
+        $(window).scroll(function(e){
+            if (Blog.loading){
+                return false;
+            }
+            if (document.documentElement.clientHeight + $(document).scrollTop() >= document.body.offsetHeight) {
+                Blog.loading = true;
+                $('#loader').show(); // a way to show the user we're working
+                Blog.loadPosts();
+            } 
+        });
+    },
+
+    loadPosts: function() {
+        // make request to backend for next batch of posts
+        $.get("/blog/?offset=" + Blog.num_loaded, function(data) {
+            if (data.indexOf("<div") >= 0) {
+                $('#loader').hide();
+                $( ".post:last" ).after(data);
+                Blog.num_loaded = $(".post").length;
+                // wait 1 sec before we allow loading posts again
+                setTimeout(function() {
+                    Blog.loading = false;
+                },1000);
+            } else {
+                // there are no more posts, remove spinner, show end of paragraph marker
+                $("#loader img").remove();
+                $("#loader").css("background-image", "url(/static/images/paragraph.png)"); 
+            }
+        });
+    }
+};
+
+$(Blog.init);
--- misodojo/apps/blog/views.py
+++ misodojo/apps/blog/views.py
@@ -1,16 +1,29 @@
-from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic import DetailView, ListView
 from .models import *
 
 
 class BlogView(ListView):
     """Blog index view"""
     model = Post
-    paginate_by = 20
+    paginate_by = 5
     http_method_names = ['get']
     template_name = 'blog/index.html'
 
     def get_queryset(self):
-         return Post.objects.select_related().filter(published=True)
+        m = Post.objects.select_related().filter(published=True)
+        if self.request.is_ajax():
+            try:
+                offset = int(self.request.REQUEST.get('offset'))
+            except:
+                offset = 0
+            if offset:
+                m = m[offset:offset + BlogView.paginate_by]
+        return m
+
+    def render_to_response(self, context):
+        if self.request.is_ajax():
+            self.template_name = 'blog/posts.html'
+        return ListView.render_to_response(self, context)
 
 
 class PostView(DetailView):
