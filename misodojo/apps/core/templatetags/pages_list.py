5941601bd19a748b325bc501d270cd4638ca4e06
Adriaan Tijsseling <adriaan@tijsseling.com>
2014-01-14 22:25:45 +0100

    Generate navbar items dynamically from flatpages.

---
+++ misodojo/apps/core/templatetags/pages_list.py
@@ -0,0 +1,20 @@
+from django import template
+from django.contrib.flatpages.models import FlatPage
+from django.core.urlresolvers import reverse
+
+register = template.Library()
+
+
+@register.simple_tag
+def pages_list(curpath):
+    """Grabs the flatpages and lists them for inclusion in the bootstrap navbar."""
+    fp = FlatPage.objects.all().values_list('title', 'url')
+    h = u''
+    for t, u in fp:
+        h += u'<li'
+        if u in curpath:
+            h += u' class="active"'
+        h += u'><a href="'
+        h += reverse('django.contrib.flatpages.views.flatpage', kwargs={'url': u})
+        h += u'">' + t + u'</a></li>'
+    return h
